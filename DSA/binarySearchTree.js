b59fbc0b484c87506908d421baf660288d1779a3
SrishtiSonam <113123593+SrishtiSonam@users.noreply.github.com>
2024-05-19 02:51:34 +0530

    Binary Search Tree

--- DSA/binarySearchTree.js
+++ DSA/binarySearchTree.js
@@ -7,6 +7,7 @@ const radius = 20;
 const frameRate = 200;
 
 function Tree() {
+
   this.root = null;
 
   this.addValue = (val) => {
@@ -23,41 +24,38 @@ function Tree() {
     ctx.font = "20px Arial";
     ctx.fillStyle = "#bbb";
     ctx.strokeStyle = "#bbb";
-
     if (this.root == null) return "empty";
     this.root.line();
     this.root.draw();
   };
+
   this.search = (val) => {
     if (this.root == null) return "empty";
     return this.root.search(val);
   };
+
 }
 
 function Node(val, x, y) {
+
   this.left = null;
   this.right = null;
   this.value = val;
+
   this.pos = {
     x: null,
     y: null,
   };
 
   this.addNode = (node) => {
-    //less => left
     if (node.value > this.value) {
-      //check if null
       if (this.left == null) {
         this.left = node;
         this.left.pos.x = this.pos.x + dist(this.level);
         this.left.pos.y = this.pos.y + gap;
         this.left.level = this.level + 1;
       } else this.left.addNode(node);
-    }
-
-    //more => right
-    else if (node.value < this.value) {
-      //check if null
+    } else if (node.value < this.value) {
       if (this.right == null) {
         this.right = node;
         this.right.pos.x = this.pos.x - dist(this.level);
@@ -69,33 +67,24 @@ function Node(val, x, y) {
 
   this.draw = (parent = null) => {
     if (this.left) this.left.draw(this);
-
     drawNode(this, "#bbb", "#202124", "#bbb");
-
     if (this.right) this.right.draw(this);
   };
 
   this.line = (parent = null) => {
     if (this.left) this.left.line(this);
-
     if (parent != null) {
-      //draw line
       ctx.beginPath();
       ctx.moveTo(parent.pos.x + radius / 2, parent.pos.y - radius / 2);
       ctx.lineTo(this.pos.x + radius / 2, this.pos.y - radius / 2);
       ctx.stroke();
     }
-
     if (this.right) this.right.line(this);
   };
 
   this.search = async (val) => {
-    //highlight
     drawNode(this, "#0482e9", "#0482e9", "#202124");
-
-    console.log("frame");
     await sleep(frameRate);
-
     if (this.value == val) console.log(this.level);
     else if (this.value > val && this.right != null)
       return this.right.search(val);
@@ -125,8 +114,6 @@ function drawNode(node, border, fill, text) {
   ctx.fill();
   ctx.stroke();
   ctx.closePath();
-
-  //text
   ctx.fillStyle = text;
   ctx.fillText(node.value, node.pos.x + 5, node.pos.y - 2);
 }
@@ -140,36 +127,37 @@ function dist(level) {
 init();
 function init() {
   tree = new Tree();
-
   random(10);
   tree.draw();
   tree.search(size);
 }
 
+// Getting the values
 function start() {
-  const goal = document.querySelector('input[type="number"]').value;
-
-  //reset
+  const goal = document.querySelector('#findingNumber').value;
   ctx.fillStyle = "#202124";
   ctx.fillRect(0, 0, canvas.height, canvas.width);
-
   tree.draw();
   tree.search(goal);
 }
 
+function generateUniqueRandomNumbers(size) {
+  const numbers = Array.from({ length: size }, (_, index) => index + 1);
+  for (let i = numbers.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+  }
+  return numbers;
+}
+
 function random() {
-  const size = document.querySelector('input[type="range"]').value;
-
+  const size = document.querySelector('#size').value;
+  const uniqueRandomNumbers = generateUniqueRandomNumbers(size);
   tree = new Tree();
-
-  //reset
   ctx.fillStyle = "#202124";
   ctx.fillRect(0, 0, canvas.height, canvas.width);
-
-
   for (let i = 0; i < size; i++) {
-    tree.addValue(Math.floor(Math.random() * size + 1));
+    tree.addValue(uniqueRandomNumbers[i]);
   }
-  
   tree.draw();
 }
